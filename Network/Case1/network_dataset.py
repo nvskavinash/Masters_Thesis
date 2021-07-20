d9e564ef44ed360be0e6f82712917fbf9c6f06f0
NVSK-Avinash <44010870+nvskavinash@users.noreply.github.com>
2021-07-20 13:36:20 -0400

    Create network_dataset.py

---
+++ Network/Case1/network_dataset.py
@@ -0,0 +1,31 @@
+'''
+
+@author: nvsk.avinash
+
+'''
+
+import sys
+a1=sys.argv[1]
+
+orig_stdout=sys.stdout
+
+sys.stdout = open('/home/nvsk/Downloads/case1/pcap/combined/output_cachealone_case1.csv', 'a')
+
+f=open('/home/nvsk/Downloads/case1/pcap/stage2/cpu100a/result'+str(a1)+'.txt', 'r').read().splitlines()
+
+count=0
+c=''
+d=''
+while(count<100):
+	if(count==0):
+		c=f[count].split(',')[0]
+		d=f[count].split(',')[1]
+	else:
+		c=c+','+f[count].split(',')[0]
+		d=d+','+f[count].split(',')[1]
+	count+=1
+print(c+','+d+',5')
+
+sys.stdout.close()
+
+sys.stdout=orig_stdout
